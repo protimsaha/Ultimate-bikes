421e202bdffeb6f9ace3c4c7b4bbdda1726a9c68
protimsaha <protimsaha750@gmail.com>
2022-05-03 20:29:11 +0600

    add and update functionality added

--- src/Pages/Home/Home/InventoryDetail/InventoryDetail.js
+++ src/Pages/Home/Home/InventoryDetail/InventoryDetail.js
@@ -1,32 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Loading from '../../../Auth/Loading';
+// import Loading from '../../../Auth/Loading';
 import './Inventory.css'
 
 const InventoryDetail = () => {
 
     const { id } = useParams()
     const [detail, setDetail] = useState({})
-    const [loading, setLoading] = useState(false)
+    const { name, quantity } = detail
 
     useEffect(() => {
-        setLoading(true)
         const url = `http://localhost:5000/bikes/${id}`
         fetch(url)
             .then(res => res.json())
             .then(data => setDetail(data))
-        setLoading(false)
-    }, [id, loading])
+    }, [id, quantity, detail])
 
-    const { name, quantity } = detail
+
+
+
 
     const handleAdd = (event) => {
-        // event.preventDefault()
-        setLoading(true)
+        event.preventDefault()
         const number = event.target.number.value;
         const newNumber = Number(number) + (quantity)
-        // console.log(number, quantity, newNumber)
-
         const url = `http://localhost:5000/bikes/${id}`
 
         fetch(url, {
@@ -41,19 +38,33 @@ const InventoryDetail = () => {
         })
             .then((response) => response.json())
             .then((json) => console.log(json));
-        setLoading(false)
-
-    }
-    if (loading) {
-        return <Loading></Loading>
     }
 
+    const updateQuant = () => {
+        const updatedQuantity = quantity - 1
+        const url = `http://localhost:5000/bikes/${id}`
+
+        fetch(url, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                updatedQuantity
+            }),
+
+        })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+    }
 
     return (
         <div className='in-detail'>
             <h2>This is invenventory detail for :{id} </h2>
             <h2>{name}</h2>
             <h3 >{quantity}</h3>
+            <button onClick={updateQuant}>Update</button>
+
             <form onSubmit={handleAdd}>
                 <input type="number" name="number" id="" />
                 <br />
