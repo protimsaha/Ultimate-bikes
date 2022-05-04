4c7e9af362321881e3f04fa8b0c86db9bc24a73a
protimsaha <protimsaha750@gmail.com>
2022-05-04 23:27:56 +0600

    delete successful

--- src/Pages/ReviewCard/ReviewCard.js
+++ src/Pages/ReviewCard/ReviewCard.js
@@ -1,12 +1,18 @@
+import axios from 'axios';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import './ReviewCard.css'
 
 const ReviewCard = ({ reviewCard }) => {
-    const navigate = useNavigate()
+    const handleDelete = id => {
+        console.log(id)
+        const url = `http://localhost:5000/bikes/${id}`
+        axios.delete(url)
+            .then(res => {
+                console.log(res)
+            })
+    }
 
-
-    const { photo, name, price, quantity } = reviewCard;
+    const { _id, photo, name, price, quantity } = reviewCard;
     return (
         <div className='container'>
             <div className="d-flex bd-highlight mb-3 review-card">
@@ -19,8 +25,8 @@ const ReviewCard = ({ reviewCard }) => {
                     <p>{quantity}</p>
                 </div>
                 <div className="ms-auto p-2 bd-highlight d-flex flex-column justify-content-center">
-                    <button onClick={navigate('/add-item')} className='btn btn-info'>Add New item</button>
-                    <button className='btn btn-danger my-3'>Delete</button>
+                    <button className='btn btn-info'>Add New item</button>
+                    <button onClick={() => handleDelete(_id)} className='btn btn-danger my-3'>Delete</button>
                 </div>
             </div>
         </div>
